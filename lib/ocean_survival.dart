2999d7557e01dd39767a9d2d05f44a73867df217
Tia <tia.paget@outlook.com>
2024-10-18 14:52:25 +0100

    added touch movement with joystick control

--- lib/ocean_survival.dart
+++ lib/ocean_survival.dart
@@ -6,7 +6,8 @@ import 'package:ocean_survival/components/player.dart';
 
 import 'components/level.dart';
 
-class OceanSurvival extends FlameGame with HasKeyboardHandlerComponents, DragCallbacks {
+class OceanSurvival extends FlameGame
+    with HasKeyboardHandlerComponents, DragCallbacks {
   late final CameraComponent cam;
   late JoystickComponent joystick;
   Player player = Player();
@@ -20,7 +21,8 @@ class OceanSurvival extends FlameGame with HasKeyboardHandlerComponents, DragCal
     // Load all assets here
     await images.loadAllImages();
     final level = Level(name: 'level-01.tmx', player: player);
-    cam = CameraComponent.withFixedResolution(world: level, width: 640, height: 360);
+    cam = CameraComponent.withFixedResolution(
+        world: level, width: 640, height: 360);
     cam.viewfinder.anchor = Anchor.topLeft;
     addAll([cam, level]);
     if (showJoystick) {
@@ -39,7 +41,8 @@ class OceanSurvival extends FlameGame with HasKeyboardHandlerComponents, DragCal
   JoystickComponent addJoystick() {
     return JoystickComponent(
       knob: SpriteComponent.fromImage(images.fromCache('hud/knob.png')),
-      background: SpriteComponent.fromImage(images.fromCache('hud/joystick.png')),
+      background:
+          SpriteComponent.fromImage(images.fromCache('hud/joystick.png')),
       margin: EdgeInsets.only(left: 32, bottom: 32),
       priority: 1,
     );
@@ -48,32 +51,45 @@ class OceanSurvival extends FlameGame with HasKeyboardHandlerComponents, DragCal
   void updateJoystickDirection(JoystickComponent joystick, Player player) {
     switch (joystick.direction) {
       case JoystickDirection.up:
+        player.verticalMovement = -1;
         //player.direction = PlayerDirection.up;
         break;
       case JoystickDirection.upLeft:
+        player.horizontalMovement = -1;
+        player.verticalMovement = -1;
         //player.direction = PlayerDirection.up;
         break;
       case JoystickDirection.upRight:
-       // player.direction = PlayerDirection.up;
+        player.horizontalMovement = 1;
+        player.verticalMovement = -1;
+        // player.direction = PlayerDirection.up;
         break;
       case JoystickDirection.downLeft:
+        player.horizontalMovement = -1;
+        player.verticalMovement = 1;
         //player.direction = PlayerDirection.down;
         break;
       case JoystickDirection.downRight:
+        player.horizontalMovement = 1;
+        player.verticalMovement = 1;
         //player.direction = PlayerDirection.down;
         break;
       case JoystickDirection.down:
+        player.verticalMovement = 1;
         //player.direction = PlayerDirection.down;
         break;
       case JoystickDirection.left:
+        player.horizontalMovement = -1;
         //player.direction = PlayerDirection.left;
         break;
       case JoystickDirection.right:
+        player.horizontalMovement = 1;
         //player.direction = PlayerDirection.right;
         break;
       case JoystickDirection.idle:
         //player.direction = PlayerDirection.none;
-
+        player.horizontalMovement = 0;
+        player.verticalMovement = 0;
         break;
     }
   }
