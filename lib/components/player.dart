74329c61d0610b2b1638148166a2ca0fd5f0282d
Tia <tia.paget@outlook.com>
2024-10-19 00:08:09 +0100

    test

--- lib/components/player.dart
+++ lib/components/player.dart
@@ -108,6 +108,8 @@ class Player extends SpriteAnimationGroupComponent
     );
   }
 
+
+
   void _updatePlayerState() {
     PlayerState playerState = PlayerState.idle;
     if (velocity.y > _gravity && !isOnGround) {
